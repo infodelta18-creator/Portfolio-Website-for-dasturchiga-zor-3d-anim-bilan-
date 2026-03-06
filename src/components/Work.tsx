6f97a06adf71fa9f59ba571df26623c2246caced
infodelta18-creator <infodelta18@gmail.com>
2026-03-06 06:28:16 +0500

    Update Work.tsx

--- src/components/Work.tsx
+++ src/components/Work.tsx
@@ -2,7 +2,7 @@ import "./styles/Work.css";
 import WorkImage from "./WorkImage";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-import { useGSAP } from "@gsap/react";
+
 
 gsap.registerPlugin(useGSAP);
 
